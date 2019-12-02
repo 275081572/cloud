4b835694dda48e32bd39413869d30796d85a6475
徐伟 <xuw@253.com>
2019-12-02 11:29:12 +0800

    新增eureka下

---
+++ ch3-1/ch3-1-config-server/src/test/java/com/example/ch31configserver/Ch31ConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.ch31configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class Ch31ConfigServerApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
